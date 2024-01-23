b66058c33db2f8ec3181ced2d2b8e87a495c4008
Luca Blessing <lucablessing@posteo.net>
2024-01-23 12:35:36 +0100

    Add icons. Make bus tile.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    component MemoryTile inherits Rectangle {
+        width: 64px;
+        height: 64px;
+        background: #3960d5;
+
+        Image {
+            source: @image-url("icons/bus.png");
+            width: parent.width;
+            height: parent.height;
+        }
+    }
+
+    export component MainWindow inherits Window {
+        MemoryTile {}
+    }
 }
